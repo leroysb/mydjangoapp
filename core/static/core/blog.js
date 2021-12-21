aa33f81f535ce9b55eec569886063c48e302c54d
leroysb <leroybuliro@gmail.com>
2021-12-21 16:19:48 +0300

    changed homepage view 2

---
+++ core/static/core/blog.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+
+    document.querySelector('.blogpage').addEventListener('load', DivControl());
+
+    function DivControl() {
+
+        document.querySelectorAll('.articlediv').forEach((articlediv)=>
+            articlediv.style.display = 'none'
+        );
+
+        const articlediv = document.querySelectorAll('.articlediv');
+        const firstload = 4
+
+        for( let i = 0; i < firstload; i++){
+            articlediv[i].style.display = 'block'
+        };
+
+        if(articlediv.length >= firstload ) {
+            document.querySelector('#loadnext').style.display='block';
+        };
+    };
+
+    const articlediv = document.querySelectorAll('.articlediv');
+    var currentindex = 4
+    
+    document.querySelector('#loadnext').onclick = ()=> {
+        
+        var next = 4;
+
+        for(var i = 0 ; i < next; i++){
+            if(currentindex >= articlediv.length) {
+                document.querySelector('#loadnext').style.display='none';
+            } else {
+                articlediv[i+currentindex].style.display = 'block';
+            }
+        }
+
+        currentindex += next
+    };
+
+
+});
