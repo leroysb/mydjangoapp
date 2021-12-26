4ca70532930c0237fd1d2769026211c40610c557
leroysb <leroybuliro@gmail.com>
2021-12-26 21:13:00 +0300

    homepage build finalized

--- core/static/core/blog.js
+++ core/static/core/blog.js
@@ -30,7 +30,6 @@ document.addEventListener('DOMContentLoaded', ()=> {
         for(var i = 0 ; i < next; i++){
             if(currentindex >= articlediv.length) {
                 document.querySelector('#loadnext').style.display='none';
-                return
             } else {
                 articlediv[i+currentindex].style.display = 'block';
             }
