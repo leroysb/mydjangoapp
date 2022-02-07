a15582a3ea94e0c145be4537a74cfb30edca60b9
leroysb <leroybuliro@gmail.com>
2022-02-07 21:17:57 +0300

    ign

--- core/static/core/podcast.js
+++ core/static/core/podcast.js
@@ -1,45 +1,44 @@
 document.addEventListener ('DOMContentLoaded', function(){
     
-    const pButton3 = document.querySelector('#pButton3');
-    const audio = document.querySelectorAll('.episodeaudio');
+    const pButton = document.querySelectorAll('#pButton');
+    const pButtonPT = document.querySelector('#pButtonPT');
+    const audio = document.querySelectorAll('#episodeaudio');
     var audioplaying = False;
     
     document.addEventListener('load', ()=> {
-        document.querySelectorAll('#pButton1').forEach((pButton1) => {
-            pButton1.alt="play episode";
-            pButton1.src="/static/core/media/playnow.png";
-            pButton1.addEventListener('click', PlayPauseEpisode());
+        document.querySelectorAll('#pButton').forEach((pButton,i) => {
+            pButton.alt="play episode";
+            pButton.src="/static/core/media/playnow.png";
+            pButton.addEventListener('click', PlayPauseEpisode());
         });
         
-        pButton3.alt="play episode";
-        pButton3.src = "/static/core/media/play.png";
-        // pButton3.addEventListener('click', PlayPauseEpisode());
+        pButtonPT.alt="play episode";
+        pButtonPT.src = "/static/core/media/play.png";
+        pButtonPT.addEventListener('click', PlayPauseEpisode());
     });
 
     //Player Controls
 
     function PlayPauseEpisode() {
-        document.querySelectorAll('#pButton1').forEach((pButton1,i) => {
+        // document.querySelectorAll('#pButton').forEach((pButton,i) => {
 
             // If audio already playing (playing=true)
             if(audioplaying == True) {
 
                 // Stop all audio
-                document.querySelectorAll('.episodeaudio').forEach((audio) => {
-                    audio.stop();
-                });
+                audio.stop();
 
                 // Reset all images to play icon
-                pButton1.alt="play episode";
-                pButton1.src="/static/core/media/playnow.png";
-                pButton3.alt="play episode";
-                pButton3.src = "/static/core/media/play.png";
+                pButton.alt="play episode";
+                pButton.src="/static/core/media/playnow.png";
+                pButtonFT.alt="play episode";
+                pButtonFT.src = "/static/core/media/play.png";
 
                 // Change selected episode icon to pause + main footer icon
-                pButton1[i].alt="pause episode";
-                pButton1[i].src="/static/core/media/pausenow.png";
-                pButton3.alt="pausey episode";
-                pButton3.src = "/static/core/media/pause.png";
+                pButton[i].alt="pause episode";
+                pButton[i].src="/static/core/media/pausenow.png";
+                pButtonPT.alt="pause episode";
+                pButtonPT.src = "/static/core/media/pause.png";
 
                 // Start audio for selected episode
                 audio[i].play();
@@ -51,10 +50,10 @@ document.addEventListener ('DOMContentLoaded', function(){
             // else, if (playing=false)
             if(audioplaying == False) {
                 // Change selected episode icon to pause + main footer icon
-                pButton1[i].alt="pause episode";
-                pButton1[i].src="/static/core/media/pausenow.png";
-                pButton3.alt="pausey episode";
-                pButton3.src = "/static/core/media/pause.png";
+                pButton[i].alt="pause episode";
+                pButton[i].src="/static/core/media/pausenow.png";
+                pButtonPT.alt="pausey episode";
+                pButtonPT.src = "/static/core/media/pause.png";
 
                 // Start audio for selected episode
                 audio[i].play();
@@ -62,7 +61,7 @@ document.addEventListener ('DOMContentLoaded', function(){
                 // set (playing=true)
                 audioplaying = True;
             };
-        });
+        // });
     };
 
     
@@ -78,4 +77,44 @@ document.addEventListener ('DOMContentLoaded', function(){
     // // timeline width adjusted for playhead
     // const timelineWidth = timeline.offsetWidth - playhead.offsetWidth;
 
+    /* menu */
+    
+    const menuBtn = document.querySelector('.menu-btn');
+    let menuOpen = false;
+    
+    menuBtn.addEventListener('click', () => {
+        const mobMenu = document.querySelector('.mobile-menu');
+        
+        if(!menuOpen){
+            menuBtn.classList.add('open');
+            mobMenu.classList.add('show');
+            menuOpen= true;
+        } else {
+            menuBtn.classList.remove('open');
+            mobMenu.classList.remove('show');
+            menuOpen = false;
+        }
+    });
+
+    /* Dropdown */
+
+    document.querySelector('.dropbtn').onclick = ()=> {
+        document.querySelector('#myDropdown').classList.toggle('show')
+    };
+
+    // Close the dropdown if the user clicks outside of the Dropdown button
+
+    window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    };
+    
 });
