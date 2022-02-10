6556aa0d019adb90094c15496c833840725ed000
leroysb <leroybuliro@gmail.com>
2022-02-10 17:20:47 +0300

    ign

--- core/views.py
+++ core/views.py
@@ -112,12 +112,12 @@ class EventList(ListView):
 
 class LegalView(ListView):
     context_object_name = 'privacy'
-    queryset = Privacy.objects.order_by('-date').first()
+    queryset = Privacy.objects.order_by('-date')
     template_name = 'core/legal.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['terms'] = Terms.objects.order_by('-date').first()
+        context['terms'] = Terms.objects.order_by('-date')
         return context
 
 def get_client_ip(request):
