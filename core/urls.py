d9e1dbf05bf1ff34c8b96cd47334dad889a435a9
leroysb <leroybuliro@gmail.com>
2022-02-12 00:58:30 +0300

    ign

--- core/urls.py
+++ core/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path("", HomepageView.as_view(), name="index"),
     path("a-z-logs", BlogView.as_view(), name="blogpage"),
     path("a-z-logs/entry-<int:pk>/<slug:slug>", ArticleView.as_view(), name="blogpost"),
-    # path("a-z-logs/entry-<int:pk>/<slug:slug>", ArticleView, name="blogpost"),
     path('kubonga-show', PodcastView.as_view(), name="podcast"),
     path("kubonga-show/s<int:s>e<int:e>/<slug:slug>", EpisodeView.as_view(), name="episode"),
     path("legal", LegalView.as_view(), name="legal"),
