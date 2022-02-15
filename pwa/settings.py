29dcdb2ba05a273981584185ab705e4014ac776e
leroysb <leroybuliro@gmail.com>
2022-02-15 20:50:50 +0300

    ign:

--- pwa/settings.py
+++ pwa/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -106,17 +106,17 @@ LOGIN_REDIRECT_URL = '/home'
 LOGOUT_REDIRECT_URL = '/home'
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn') # Local
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # For heroku static
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static_cdn'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn') # Local
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # For heroku static
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static_cdn'),
+# ]
 
-# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 # For Heroku Static Serving
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
-WHITENOISE_MANIFEST_STRICT = False
-WHITENOISE_KEEP_ONLY_HASHED_FILES = True
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
+# WHITENOISE_MANIFEST_STRICT = False
+# WHITENOISE_KEEP_ONLY_HASHED_FILES = True
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
@@ -142,5 +142,5 @@ CKEDITOR_CONFIGS = {
 }
 
 # Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
+# import django_heroku
+# django_heroku.settings(locals())
---
+++ static_cdn/core/podcast.js
@@ -0,0 +1,87 @@
+document.addEventListener ('DOMContentLoaded', function(){
+
+    // var element = document.createElement('div');
+    // element.classList.add('epi-play');
+    // document.querySelector('.podcast-epi').appendChild(element);
+
+    $(window).load(function(){
+        $('.epi-play').append('<img id="pButton" src="/static/core/media/playnow.png" style="width: 100%; height: 100%;"></img>')
+    })
+
+    // const pgSetup = (function() {
+    //     // var pButton = new Image();
+    //     var pButton = document.createElement('img')
+    //     pButton.src = "/static/core/media/playnow.png";
+    //     pButton.height = "100%";
+    //     pButton.width = "100%";
+    //     document.querySelector('.epi-play').appendChild(pButton);
+    //     pButton.addEventListener('click', PlayPause());
+    
+    //     // var pButtonFT = new Image();
+    //     // pButtonFT.src = "/static/core/media/play.png";
+    //     // pButtonFT.height = "100%";
+    //     // pButtonFT.width = "100%";
+    //     // document.querySelector('.footerimage').appendChild(pButtonFT);
+    //     // pButtonFT.addEventListener('click', PlayPause());
+    // }());
+
+    // pgSetup()
+
+    const audio = document.querySelectorAll('#audio');
+    let audioplaying = false;
+
+    //Player Controls
+
+    function PlayPause() {
+        pButton.forEach((pButton,i) => {
+
+            // If audio already playing (playing=true)
+            if(audioplaying !== true) {
+
+                // Stop all audio
+                audio.stop()
+
+                // Reset all images to play icon
+                pButton.src="/static/core/media/playnow.png"
+                pButtonFT.src = "/static/core/media/play.png"
+
+                // Change selected episode icon to pause + main footer icon
+                pButton[i].src="/static/core/media/pausenow.png"
+                pButtonFT.src = "/static/core/media/pause.png"
+
+                // Start audio for selected episode
+                audio[i].play()
+
+                // set (playing=true)
+                audioplaying = true
+            }
+
+            // else, if (playing=false)
+            if(audioplaying == false) {
+                // Change selected episode icon to pause + main footer icon
+                pButton[i].src="/static/core/media/pausenow.png"
+                pButtonFT.src = "/static/core/media/pause.png"
+
+                // Start audio for selected episode
+                audio[i].play()
+
+                // set (playing=true)
+                audioplaying = true
+            }
+        })
+    }
+
+    
+    // const podcast = document.querySelector('#podcast') // id for audio element
+    // const player = document.getElementsByClassName('player')
+
+    // const playhead = document.getElementById('playhead') // playhead
+    // const timeline = document.getElementById('timeline') // timeline
+    // const timerWrapper = document.querySelector('.timer')
+    // const timer = document.querySelector('.timer span')
+    // const timerBar = document.querySelector('.timer div')
+
+    // // timeline width adjusted for playhead
+    // const timelineWidth = timeline.offsetWidth - playhead.offsetWidth
+    
+})
