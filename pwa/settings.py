2aa85f2ef6fd184787410d871199a69453a93217
leroysb <leroybuliro@gmail.com>
2022-02-16 23:08:08 +0300

    ign

--- pwa/settings.py
+++ pwa/settings.py
@@ -114,7 +114,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn') # Local
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 # For Heroku Static Serving
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # WHITENOISE_MANIFEST_STRICT = False
 # WHITENOISE_KEEP_ONLY_HASHED_FILES = True
 
