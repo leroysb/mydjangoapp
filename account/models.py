6d8ceea8af6690102ea8da6411ed3bac9f2d4e0a
leroysb <leroybuliro@gmail.com>
2022-02-20 22:30:31 +0300

    ign

--- account/models.py
+++ account/models.py
@@ -11,20 +11,21 @@ from django.utils.translation import gettext_lazy as _
 
 class UserManager(BaseUserManager):
     
-    def create_user(self, email, password=None):
+    def create_user(self, email, alias, password=None):
 
         if not email:
             raise ValueError('Users must have an email address')
 
-        user = self.model( email=self.normalize_email(email), )
+        user = self.model( email=self.normalize_email(email), alias=self.alias)
         user.set_password(password)
         user.save(using=self._db)
         return user
     
-    def create_superuser(self, email, password):
+    def create_superuser(self, email, alias, password):
         
         user = self.create_user(
             email,
+            alias=alias,
             password=password,
         )
         user.is_superuser = True
