29b5c41178b38c21536eba27c737b586c28196af
leroysb <leroybuliro@gmail.com>
2022-03-31 18:29:36 +0300

    authentication parameters upgrade

--- core/views.py
+++ core/views.py
@@ -4,21 +4,21 @@ from .models import *
 
 # Error views.
 
-# def page404 (request, exception):
-#     return render(request, 'core/404.html')
+def page404 (request, exception):
+    return render(request, 'core/404.html')
 
-# def page500 (request):
-#     return render(request, 'core/serverError.html')
+def page500 (request):
+    return render(request, 'core/serverError.html')
 
-# class HomepageView(TemplateView):
-#     template_name = 'core/homepage.html'
+class HomepageView(TemplateView):
+    template_name = 'core/homepage.html'
 
-# class LegalView(ListView):
-#     context_object_name = 'privacy'
-#     queryset = Privacy.objects.order_by('-date')
-#     template_name = 'core/legal.html'
+class LegalView(ListView):
+    context_object_name = 'privacy'
+    queryset = Privacy.objects.order_by('-date')
+    template_name = 'core/legal.html'
 
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['terms'] = Terms.objects.order_by('-date')
-#         return context
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['terms'] = Terms.objects.order_by('-date')
+        return context
--- pwa/settings.py
+++ pwa/settings.py
@@ -24,7 +24,7 @@ INSTALLED_APPS = [
 
     # My app
     'account',
-    # 'core',
+    'core',
 
     # Third party apps
     'ckeditor',
