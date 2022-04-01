6c26809138677bc34bb84ff7edc2d1122425c9f9
leroysb <leroybuliro@gmail.com>
2022-04-01 17:16:50 +0300

    UX redisign

--- account/controllers/subscribe.py
+++ account/controllers/subscribe.py
@@ -32,7 +32,7 @@ class subscribeForm(forms.ModelForm):
         alias = self.cleaned_data.get('alias')
 
         if not User.objects.filter(alias__iexact=alias).exists():
-            raise forms.ValidationError (f'{alias} + " is not available"')
+            raise forms.ValidationError (f'{alias} is not available')
         return alias
 
     def save(self, commit=True):
