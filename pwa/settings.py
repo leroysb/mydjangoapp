01543aac6dfb50d6a8268f167033826ea41b32f6
leroysb <leroybuliro@gmail.com>
2022-04-10 14:21:46 +0300

    ign

--- pwa/settings.py
+++ pwa/settings.py
@@ -154,12 +154,11 @@ CKEDITOR_CONFIGS = {
 #SMPT Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_SSL = True
 EMAIL_PORT = 465
 EMAIL_HOST_USER = os.environ.get('PWAEmailUser')
 EMAIL_HOST_PASSWORD = os.environ.get('PWAEmailPwd')
-DEFAULT_FROM_EMAIL  = 'admin@my-site.com'
-EMAIL_USE_TLS = True
-# EMAIL_USE_SSL = True
+# EMAIL_USE_TLS = True
 # EMAIL_TIMEOUT
 # EMAIL_SSL_KEYFILE
 # EMAIL_SSL_CERTFILE
