3c47e5239e00f5d76529e6f01cd8eff8d034a050
leroysb <leroybuliro@gmail.com>
2022-04-23 23:29:51 +0300

    hp ux change

--- pwa/settings.py
+++ pwa/settings.py
@@ -113,9 +113,9 @@ SESSION_TIMEOUT_REDIRECT = "/"
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
