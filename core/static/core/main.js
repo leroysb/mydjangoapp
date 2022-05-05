21190f0894c55c91939853e631637e4dbf9dbc72
leroysb <leroybuliro@gmail.com>
2022-05-05 19:21:32 +0300

    nav ux modifications

--- core/static/core/main.js
+++ core/static/core/main.js
@@ -1,21 +1,4 @@
 document.addEventListener('DOMContentLoaded', function(){
-    // MENU
-    const menuBtn = document.querySelector('.menu-btn');
-    let menuOpen = false;
-    
-    menuBtn.addEventListener('click', () => {
-        const mobMenu = document.querySelector('.mobile-menu');
-        
-        if(!menuOpen){
-            menuBtn.classList.add('open');
-            mobMenu.classList.add('show');
-            menuOpen= true;
-        } else {
-            menuBtn.classList.remove('open');
-            mobMenu.classList.remove('show');
-            menuOpen = false;
-        }
-    });
 
     // FEEDBACK MODAL
     document.addEventListener('click', (event)=> {
