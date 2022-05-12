e9afaf313ccf0c7de34dade2ae2877b2098b7478
leroysb <leroybuliro@gmail.com>
2022-05-12 12:48:59 +0300

    ux mod

--- core/static/core/main.js
+++ core/static/core/main.js
@@ -21,46 +21,6 @@ document.addEventListener('DOMContentLoaded', function(){
             $(".rating span").removeClass('checked');
             $(this).parent().addClass('checked');
         });
-    });
-
-    // BLOG PAGE
-    document.querySelector('.blogpage').addEventListener('load', ()=> {
-    
-        const articles = document.querySelectorAll('.articlediv');
-        const firstload = 8
-
-        document.querySelectorAll('.articlediv').forEach((article)=>
-            article.style.display = 'none'
-        );
-    
-        if(articles.length >= firstload ) {
-            document.querySelector('#showbtn').style.display='flex';
-        } else {
-            document.querySelector('#showbtn').style.display='none';
-        };
-    
-        for(let i = 0; i < firstload; i++){
-            articles[i].style.display = 'block';
-        };
-    });
-
-
-    const articlediv = document.querySelectorAll('.articlediv');
-    var currentindex = 8
-    
-    document.querySelector('#loadnext').onclick = ()=> {
-        
-        var next = 8;
-
-        for(var i = 0 ; i < next; i++){
-            if(currentindex >= articlediv.length) {
-                document.querySelector('#showbtn').style.display='none';
-            } else {
-                articlediv[i+currentindex].style.display = 'block';
-            }
-        }
-
-        currentindex += next;
-    };
+    }); 
 
 });
